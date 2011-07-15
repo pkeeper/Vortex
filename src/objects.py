d9da002b0133867bb6b24bf9896ed52c7318f3d4
Artem melanich <pkeeper.shark@mail.ru>
2011-07-16 01:04:50 +0300

    Ship Engines basic implementation

--- src/objects.py
+++ src/objects.py
@@ -9,6 +9,37 @@
 
 debug = True
 
+class Engine(object):
+    max_thrust  =   400    #Not Zero
+    min_thrust  =   0       #May be negative value
+    current_force      =   10
+    vector      =   (0.0,1.0,0.0)   #Default force vector
+    
+    @property
+    def thrust(self):
+        print "thrust get" 
+        print self.current_force
+        return self.current_force/(self.max_thrust/100.0)
+    
+    @thrust.setter
+    def thrust(self,output):
+        
+        print "thrust set"
+        if output > 100: output = 100
+        elif output <0 : output = 0
+        self.current_force = self.min_thrust + output*((self.max_thrust - self.min_thrust)/100.0)
+        
+    def applyForce(self):
+        if debug : print self.current_force
+        force_vec = map(lambda x:x*self.current_force, self.vector)
+        self.body.addForce(force_vec)
+    
+    def __init__(self,body,vector=(0.0,1.0,0.0)):
+        self.body = body
+        self.vector = vector
+        
+        
+
 class SimpleObject(object):
     """Abstract class for all physics bodies.
     """
@@ -62,15 +93,29 @@ class Ship(SimpleObject):
     leftthrust = False
     rightthrust = False
     
-#    def __init__(self, *args, **kwargs):
-#        PhysicsBody.__init__(self, *args, **kwargs)
-#        self.type = 'ship'
+    def __init__(self, *args, **kwargs):
+        SimpleObject.__init__(self, *args, **kwargs)
+        self.gravity_engine = Engine(self.body,(0.0,1.0,0.0))
+        self.nose_engine = Engine(self.body,(1.0,0.0,0.0))
+        self.back_engine = Engine(self.body,(-1.0,0.0,0.0))
         
     def calculate(self):
-        print self.position
-        if self.upthrust: self.body.addForce((0,1900,0))
-        if self.leftthrust: self.body.addForce((-300,0,0))
-        if self.rightthrust: self.body.addForce((100,0,0))
+        if debug: print self.position
+        #set up engines
+        if self.upthrust:
+            print "upthrust" 
+            self.gravity_engine.thrust = self.gravity_engine.thrust+0.5
+        if self.downthrust: self.gravity_engine.thrust = self.gravity_engine.thrust-0.5
+        if self.rightthrust: self.nose_engine.thrust = self.nose_engine.thrust+1
+        else:   self.nose_engine.thrust = 0
+        if self.leftthrust:self.back_engine.thrust = self.back_engine.thrust+1
+        else:   self.back_engine.thrust = 0
+        
+        #apply engine forces
+        self.gravity_engine.applyForce()
+        self.nose_engine.applyForce()
+        self.back_engine.applyForce()
+        
         #set no rotation
         self.rotation = (0, -1, 0, 1, 0, 0, 0, 0, 1)
         
